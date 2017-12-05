05fb37578a57b90458408ec6ddbf08fbd5e7c28f
Milad Nekooei <mi.nekooei@gmail.com>
2017-12-05 18:03:34 +0330

    Wrap Application in BrowserRouter & Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter }  from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider >
+      <App />
+    </Provider>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
