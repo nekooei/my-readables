f7e2484730011290482a9e4cacc705b285a400ab
milad <mi.nekooei@gmail.com>
2017-12-07 00:39:50 +0330

    Load Store to provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducers from './reducers'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { BrowserRouter }  from 'react-router-dom'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducers,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
+
 ReactDOM.render(
   <BrowserRouter>
-    <Provider >
+    <Provider store={store} >
       <App />
     </Provider>
   </BrowserRouter>
