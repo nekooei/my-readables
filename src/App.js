0065a0dde949a2c61df728ecd6982814c536a624
milad <mi.nekooei@gmail.com>
2017-12-07 01:32:51 +0330

    Load New Post Component in Main Application

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-
+import {Route, Switch} from "react-router-dom";
+import Home from "./components/Home";
+import NewPost from './components/newPost'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/createNewPost" render={ ({history}) => (
+                    <NewPost history={history}/>
+                    )}/>
+            </Switch>
+        );
+    }
 }
+
+
 
 export default App;
