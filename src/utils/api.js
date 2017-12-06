705919dc00e12ca068ad19cdab923ec75fe3da2a
Milad Nekooei <mi.nekooei@gmail.com>
2017-12-06 19:56:00 +0330

    Compelete APIs methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,8 @@ const headers = {
     "Content-Type": "application/json"
 };
 
+const uuid = () => `${Date.now()}-${Math.random().toString(36).substr(-8)}`
+
 const getUrl = (route) => `${BASE_URL}${route}`;
 
 export const fetchCategories = () => (
@@ -11,21 +13,79 @@ export const fetchCategories = () => (
       .then(res => res.json())
 );
 
+export const addPost = ({title, body, author, category}) => (
+    fetch(
+        getUrl('/posts'),
+        {
+            method: "POST",
+            headers,
+            body: JSON.stringify({
+                id: uuid(),
+                timestamp: Date.now(),
+                author,
+                body,
+                title,
+                category
+            })
+        }
+    ).then(res => res.json())
+)
+
+export const deletePost = (id) => (
+    fetch(
+        getUrl(`/posts/${id}`),
+        {
+            method: "DELETE",
+            headers
+        }
+    ).then(res => res.json())
+)
+
+export const editPost = ({id, title, body}) => (
+    fetch(
+        getUrl(`/posts/${id}`),
+        {
+            method: "PUT",
+            headers,
+            body: JSON.stringify({
+                title,
+                body
+            })
+        }
+    ).then(res => res.json())
+)
+
 export const fetchPosts = (category = null) => {
     const url = category ? `/${category}/posts` : "/posts";
     return fetch(getUrl(url), {headers})
       .then(res => res.json())
-};
+}
 
 export const fetchPost = (id) => (
     fetch(getUrl(`/posts/${id}`), {headers})
       .then(res => res.json())
-);
+)
 
 export const fetchComments = (id) => (
     fetch(getUrl(`/posts/${id}/comments`), {headers})
       .then(res => res.json())
-);
+)
+
+export const addComment = ({ postId, author, body }) => (
+    fetch(
+        getUrl('/comments'), {
+            method: "POST",
+            headers,
+            body: JSON.stringify({
+                id: uuid(),
+                timestamp: Date.now(),
+                parentId: postId,
+                author,
+                body
+            })
+        }
+    ).then(res => res.json())
+)
 
 export const votePost = ({ id, vote }) => {
     const option = vote === 1 ? "upVote" : "downVote";
@@ -37,7 +97,7 @@ export const votePost = ({ id, vote }) => {
             body: JSON.stringify({option})
         }
     ).then(res => res.json())
-};
+}
 
 export const voteComment = ({ id, vote }) => {
     const option = vote === 1 ? "upVote" : "downVote";
@@ -49,4 +109,26 @@ export const voteComment = ({ id, vote }) => {
             body: JSON.stringify({option})
         }
     ).then(res => res.json())
-};
+}
+
+export const editComment = ({ id, body }) => (
+    fetch(
+        getUrl(`/comments/${id}`), {
+            method: "PUT",
+            headers,
+            body: JSON.stringify({
+                timestamp: Date.now(),
+                body
+            })
+        }
+    ).then(res => res.json())
+)
+
+export const deleteComment = (id) => (
+    fetch(
+        getUrl(`/comments/${id}`), {
+            method: "DELETE",
+            headers
+        }
+    ).then(res => res.json())
+)
