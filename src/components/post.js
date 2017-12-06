2f68397218dbb62b03dfc5c0b7426ad91d4de840
milad <mi.nekooei@gmail.com>
2017-12-07 00:39:23 +0330

    Show Overview of a post

---
+++ src/components/post.js
@@ -0,0 +1,54 @@
+/**
+ * Created by milad on 12/6/17.
+ */
+import React, {Component} from 'react'
+import {Row, Col, Divider} from 'react-materialize'
+import propTypes from 'prop-types'
+import Moment from 'react-moment'
+import {Link} from "react-router-dom";
+
+
+class Post extends Component {
+    render() {
+        const {title, timestamp, body, author, category, voteScore, id} = this.props
+        return (
+            <Link to={`/post/${id}`}>
+                <Row className="card-panel hoverable">
+                    <Col m={12}>
+                        <h4>
+                            {title}
+                        </h4>
+                        <Divider/>
+                        <p>
+                            {body}
+                        </p>
+                        <Divider/>
+                        <Row>
+                            <Col m={4}>
+                                author: {author}
+                            </Col>
+                            <Col m={4}>
+                                category: {category}
+                            </Col>
+                            <Col m={4}>
+                                <Moment format="MMM Do YYYY">{timestamp}</Moment>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Link>
+        );
+    }
+}
+
+Post.propTypes = {
+    title: propTypes.string.isRequired,
+    timestamp: propTypes.number.isRequired,
+    body: propTypes.string.isRequired,
+    author: propTypes.string.isRequired,
+    category: propTypes.string.isRequired,
+    voteScore: propTypes.number.isRequired
+};
+Post.defaultProps = {};
+
+export default Post
