b96f905c63918f28db058d8e06955c7289fb9efd
milad <mi.nekooei@gmail.com>
2017-12-07 04:10:46 +0330

    Modify Comment reducers

--- src/reducers/comment.js
+++ src/reducers/comment.js
@@ -3,36 +3,24 @@
  */
 import {ADD_COMMENT, DELETE_COMMENT, FETCH_COMMENTS, UPDATE_COMMENT} from "../actions/comment";
 
-const initialState = {};
+const initialState = [];
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case FETCH_COMMENTS:
-            return {
-                ...state,
-                [action.postId]: action.comments
-            };
-
+            return action.comments
         case ADD_COMMENT:
-            return {
+            return [
                 ...state,
-                [action.comment.parentId]: [...(state[action.comment.parentId] || []), action.comment]
-            };
+                action.comment
+            ]
 
         case DELETE_COMMENT:
-            return {
-                ...state,
-                [action.comment.parentId]: state[action.comment.parentId].filter((c) => c.id !== action.comment.id)
-            };
+            return state.filter(comment => comment.id !== action.comment.id)
 
         case UPDATE_COMMENT:
-            const parentId = action.comment.parentId;
             const newComment = action.comment;
-            return {
-                ...state,
-                [parentId]: state[parentId].map((c) => ( c.id !== newComment.id ? c : newComment))
-
-            };
+            return state.map(comment => comment.id === newComment.id ? newComment : comment)
         default:
             return state;
     }
