1c9d03a0557b61cb8f7b8c980a6d13a62ffcbf2b
Milad Nekooei <mi.nekooei@gmail.com>
2017-12-09 18:21:02 +0330

    show Comment Count and redirect to Not found page when change url

--- src/components/postDetail.js
+++ src/components/postDetail.js
@@ -9,6 +9,7 @@ import {voteUpPost, voteDownPost, deletePost, editPost, getPost} from '../action
 import {addComment, fetchComments} from '../actions/comment'
 import Moment from "react-moment";
 import Comment from "./comment";
+import NotFound from './notFound'
 
 class PostDetail extends Component {
 
@@ -57,73 +58,80 @@ class PostDetail extends Component {
     }
 
     render() {
-        const {title, body, author, category, timestamp, voteScore} = this.props.post
+        const {title, body, author, category, timestamp, voteScore, commentCount} = this.props.post
+        console.log(this.props.post);
+        if(!this.props.post.hasOwnProperty('id')) {
+          return <NotFound/>
+        }
         return (
-            <Row >
-                <Col m={4}>
+          <div>
+            <Header/>
+              <Row >
+                  <Col m={4}>
 
-                    <br/>
-                    <Col m={6}>
-                        <Button className="red" icon='thumb_down' onClick={this.voteDownPost}/>
-                    </Col>
-                    <Col m={6}>
-                        <Button className="blue" icon='thumb_up' onClick={this.voteUpPost}/>
-                    </Col>
-                    <Col m={6}>
-                        <Button className="orange" icon='close' onClick={this.deletePost}/>
-                    </Col>
-                    <Col m={6}>
-                        <Button className="green" icon='settings' onClick={this.editPost}/>
-                    </Col>
+                      <br/>
+                      <Col m={6}>
+                          <Button className="red" icon='thumb_down' onClick={this.voteDownPost}/>
+                      </Col>
+                      <Col m={6}>
+                          <Button className="blue" icon='thumb_up' onClick={this.voteUpPost}/>
+                      </Col>
+                      <Col m={6}>
+                          <Button className="orange" icon='close' onClick={this.deletePost}/>
+                      </Col>
+                      <Col m={6}>
+                          <Button className="green" icon='settings' onClick={this.editPost}/>
+                      </Col>
 
-                    <Col m={12}>
-                        <Button className="green" icon='arrow_left' onClick={this.back}/>
-                    </Col>
-
-
-                </Col>
-                <Col m={8} >
-                    <h5>{title}</h5>
-                    <Divider/>
-                    <p>
-                        {body}
-                    </p>
-                    <Row>
-                        <Col m={3}>
-                            author: {author}
-                        </Col>
-                        <Col m={3}>
-                            category: {category}
-                        </Col>
-                        <Col m={3}>
-                            Date: <Moment format="MMM Do YYYY">{timestamp}</Moment>
-                        </Col>
-                        <Col m={3}>
-                            vote: {voteScore}
-
-                        </Col>
-                    </Row>
-                    <Divider/>
-
-                    <h5>Comments</h5>
-                    <Row>
-                        {this.props.comments.map(comment => comment.deleted || comment.parentDeleted ?  null : (
-                            <Comment key={comment.id} id={comment.id} body={comment.body} author={comment.author}
-                                     timestamp={comment.timestamp} voteScore={comment.voteScore} history={this.props.history}/>
-                        ))}
-                        <Divider/>
-                        <Row>
-                            <h5>Insert Comment</h5>
-                            <Input m={12} label="author" onChange={(event, value) => this.authorComment = value}/>
-                            <Input m={12} label="body" onChange={(event, value) => this.bodyComment = value}/>
-                            <Button onClick={this.saveComment}>Save</Button>
-                            <p>{this.state.error ? "please fill all fields" : ""}</p>
-                        </Row>
-                    </Row>
-                </Col>
+                      <Col m={12}>
+                          <Button className="green" icon='arrow_left' onClick={this.back}/>
+                      </Col>
 
 
-            </Row>
+                  </Col>
+                  <Col m={8} >
+                      <h5>{title}</h5>
+                      <Divider/>
+                      <p>
+                          {body}
+                      </p>
+                      <Row>
+                          <Col m={3}>
+                              author: {author}
+                          </Col>
+                          <Col m={3}>
+                              category: {category}
+                          </Col>
+                          <Col m={3}>
+                              Date: <Moment format="MMM Do YYYY">{timestamp}</Moment>
+                          </Col>
+                          <Col m={3}>
+                              vote: {voteScore}
+
+                          </Col>
+                      </Row>
+                      <Divider/>
+
+                      <h5>Comments : {commentCount}</h5>
+                      <Row>
+                          {this.props.comments.map(comment => comment.deleted || comment.parentDeleted ?  null : (
+                              <Comment key={comment.id} id={comment.id} body={comment.body} author={comment.author}
+                                       timestamp={comment.timestamp} voteScore={comment.voteScore} history={this.props.history}/>
+                          ))}
+                          <Divider/>
+                          <Row>
+                              <h5>Insert Comment</h5>
+                              <Input m={12} label="author" onChange={(event, value) => this.authorComment = value}/>
+                              <Input m={12} label="body" onChange={(event, value) => this.bodyComment = value}/>
+                              <Button onClick={this.saveComment}>Save</Button>
+                              <p>{this.state.error ? "please fill all fields" : ""}</p>
+                          </Row>
+                      </Row>
+                  </Col>
+
+
+              </Row>
+            </div>
         );
     }
 }
